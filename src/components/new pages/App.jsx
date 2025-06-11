7823dd08adbbdb8ae07194c094c4cbb6c221764c
Darshan9121 <darshanhiragar44@gmail.com>
2025-06-11 11:21:11 +0530

    New Design Changes Merged

--- src/components/new pages/App.jsx
+++ src/components/new pages/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HashRouter } from 'react-router-dom';
 import React, { useState } from 'react';
 import "./index.css";
 import Home from "./Pages/Home";
@@ -32,7 +31,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
           <footer>
             <center>
               <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
-                <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+              <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
                 © 2025{" "}
                 <a href="https://flowbite.com/" className="hover:underline">
                   Shrinav
@@ -53,13 +52,13 @@ const ProjectPageLayout = () => (
     <footer>
       <center>
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
-         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-                © 2025{" "}
-                <a href="https://flowbite.com/" className="hover:underline">
-                  Shrinav
-                </a>
-                . All Rights Reserved.
-              </span>
+        <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+          © 2023{" "}
+          <a href="https://flowbite.com/" className="hover:underline">
+            Shrinav
+          </a>
+          . All Rights Reserved.
+        </span>
       </center>
     </footer>
   </>
@@ -69,12 +68,12 @@ function App() {
   const [showWelcome, setShowWelcome] = useState(true);
 
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage showWelcome={showWelcome} setShowWelcome={setShowWelcome} />} />
         <Route path="/project/:id" element={<ProjectPageLayout />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
